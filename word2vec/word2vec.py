3d5cdae193890c641a6dbcffe834c25acfff4c9e
alessio rea <alexei.80@hotmail.fr>
2021-04-28 12:00:52 +0200

    progress word2vec embedding model

--- word2vec/word2vec.py
+++ word2vec/word2vec.py
@@ -6,3 +6,10 @@ from gensim.models import Phrases
 from gensim.models.phrases import Phraser
 
 
+if __name__ == "__main__":
+
+    file_path = os.path.join(os.getcwd(), 'word2vec', "reviews_combined_amazon.csv")
+    combined_reviews = pd.read_csv(file_path)
+    print(combined_reviews.head())
+
+    
