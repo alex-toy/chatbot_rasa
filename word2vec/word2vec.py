d7e61bf0a3d8674f86b276612f1492b2c292cbdb
alessio rea <alexei.80@hotmail.fr>
2021-04-28 12:05:00 +0200

    progress word2vec embedding model

--- word2vec/word2vec.py
+++ word2vec/word2vec.py
@@ -10,6 +10,14 @@ if __name__ == "__main__":
 
     file_path = os.path.join(os.getcwd(), 'word2vec', "reviews_combined_amazon.csv")
     combined_reviews = pd.read_csv(file_path)
-    print(combined_reviews.head())
+    #print(combined_reviews.head())
 
+    reviews_title_text_list = combined_reviews['review_title']. \
+        apply(lambda x:str(x)) + " " + combined_reviews['review_text']. \
+        apply(lambda x:str(x)). \
+        to_list()
+
+    sentence_stream = [doc.split(" ") for doc in reviews_title_text_list]
+
+    print(sentence_stream[0][0:10])
     
